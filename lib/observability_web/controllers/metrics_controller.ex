fe29d85581e7204810cfaa20c6a87287586ef3ca
Kate Rezentes <kfrezentes@gmail.com>
2025-08-24 04:29:29 -0400

    refactor: remove cowboy

--- lib/observability_web/controllers/metrics_controller.ex
+++ lib/observability_web/controllers/metrics_controller.ex
@@ -3,10 +3,8 @@ defmodule ObservabilityWeb.MetricsController do
   require Logger
 
   def index(conn, _params) do
-    metrics = TelemetryMetricsPrometheus.Core.scrape()
-
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(200, metrics)
+    name = :prometheus_metrics
+    metrics = TelemetryMetricsPrometheus.Core.scrape(name)
+    text(conn, metrics)
   end
 end
--- lib/observability_web/telemetry.ex
+++ lib/observability_web/telemetry.ex
@@ -12,7 +12,7 @@ defmodule ObservabilityWeb.Telemetry do
       # Telemetry poller will execute the given period measurements
       # every 10_000ms. Learn more here: https://hexdocs.pm/telemetry_metrics
       {:telemetry_poller, measurements: periodic_measurements(), period: 10_000},
-      {TelemetryMetricsPrometheus, metrics: metrics()}
+      {TelemetryMetricsPrometheus.Core, metrics: metrics(), name: :prometheus_metrics}
       # Add reporters as children of your supervision tree.
       # {Telemetry.Metrics.ConsoleReporter, metrics: metrics()}
     ]
